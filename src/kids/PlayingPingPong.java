bbd20b5fb27a5457e8ebf82a4f808847d4b9c88c
hurayra <khanjada.hurayra.qa@gmail.com>
2019-05-19 00:29:17 -0400

    this is practice test

---
+++ src/kids/PlayingPingPong.java
@@ -0,0 +1,25 @@
+package kids;
+
+public class PlayingPingPong {
+// this is a practice test
+    public static void main(String[]args){
+
+        int house=20;
+         switch (house){
+             case 10:
+                 System.out.println("house is grater than 20");
+                 break;
+             case 15:
+                 System.out.println("house is less than 20");
+                 break;
+             case 20:
+                 System.out.println("house is equal to20");
+                 break;
+             default:
+
+
+
+
+         }
+    }
+}
